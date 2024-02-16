6677fe95e35d2789d2eb8932f74342aec965c146
jaddie-lorzano <jaddielorzano@gmail.com>
2024-02-16 20:52:59 +0800

    Creating Database using sqlite

---
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -0,0 +1,20 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContexts
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointOfInterests { get; set;}
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options) { }
+
+        // one way to connect db context to a sqlite db
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
