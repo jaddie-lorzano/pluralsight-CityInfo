9ff3500110a5d7dfd5eeae3a77affdb4a6772058
jaddie-lorzano <jaddielorzano@gmail.com>
2024-02-16 15:40:53 +0800

    Add City and Points of Interest Entities for db models

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,23 @@
+﻿using CityInfo.API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class City(string name)
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = name;
+
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } 
+            = new List<PointOfInterestDto>();
+    }
+}
---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest(string name)
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = name;
+
+        public string? Description { get; set; }
+
+        [ForeignKey(nameof(CityId))]
+        public City? City { get; set; }
+
+        public int CityId { get; set; }
+    }
+}
